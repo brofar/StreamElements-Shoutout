2e5359551bb73ab80304765085695bc3bf5a25d1
Behfar Lotfizadeh <behfar@otafest.com>
2020-08-08 13:34:53 -0600

    Formatting

--- js.js
+++ js.js
@@ -1,36 +1,37 @@
+// Create variables to represent StreamElements' pseudovariables
+const notificationTime = { notificationTime };
+const customCommand = '{customCommand}';
+const videoFile = "{shoutVideo}";
+const videoVolume = { shoutVideoVolume };
+const soundFile = "{shoutAudio}";
+const soundVolume = { shoutAudioVolume };
+const shoutTopText = "{shoutTopText}";
+const shoutBotText = "{shoutBotText}";
+
 // Some global variables
-let debugOutput		 = false;
-let customCommand 	 = '{customCommand}';
-let shoutoutDuration = {notificationTime} * 1000;
+let debugOutput = false;
+let shoutoutDuration = notificationTime * 1000;
+let service = "twitch";
+let avatar_image = document.getElementById("avatar_image");
+let text_main = document.getElementById("textMain");
+let text_sub = document.getElementById("textSub");
 
-let videoFile 		 = "{shoutVideo}";
-let videoVolume 	 = {shoutVideoVolume};
+let q = new Queue();
 
-let soundFile		 = "{shoutAudio}";
-let soundVolume		 = {shoutAudioVolume};
-
-let service 		 = "twitch";
-
-let avatar_image 	 = document.getElementById("avatar_image");
-let text_main	 	 = document.getElementById("textMain");
-let text_sub	 	 = document.getElementById("textSub");
-
-let q 				 = new Queue();
-
-let messages 		 = ["Probably a decent human.", 
-                        "A featherless biped.", 
-                        "Cleans up well.",
-                        "This one is good.",
-                        "Probably not a cannibal.",
-                        "Has so much potential.",
-                        "Cute.",
-                        "Better than bubble wrap.",
-                        "Has a smile",
-                        "Inspiring",
-                        "A gift to Twitch",
-                        "A great parent",
-                        "Their aura is strong."
-                       ];
+let messages = ["Probably a decent human.",
+  "A featherless biped.",
+  "Cleans up well.",
+  "This one is good.",
+  "Probably not a cannibal.",
+  "Has so much potential.",
+  "Cute.",
+  "Better than bubble wrap.",
+  "Has a smile",
+  "Inspiring",
+  "A gift to Twitch",
+  "A great parent",
+  "Their aura is strong."
+];
 
 window.addEventListener('onWidgetLoad', (obj) => {
 
@@ -40,7 +41,7 @@ window.addEventListener('onWidgetLoad', (obj) => {
 window.addEventListener('onEventReceived', async (obj) => {
   // Event listener code used from René Chiquete's Awesome Shoutouts 1.3.
   // https://www.youtube.com/watch?v=zGFhhFit9WQ
-  
+
   // If it's not a chat event, return.
   if (obj.detail.listener !== "message") return;
 
@@ -68,21 +69,21 @@ window.addEventListener('onEventReceived', async (obj) => {
   let isBroadcaster = (badge1 === 'broadcaster');
   let isModUp = isMod || isBroadcaster;
 
-  if (command.toLowerCase() === customCommand && isModUp ) {
-    
-	debug(`Received command "${command}" with target "${target}".`);
-    
+  if (command.toLowerCase() === customCommand && isModUp) {
+
+    debug(`Received command "${command}" with target "${target}".`);
+
     // Remove '@' from the name, in the case it was sent in that way from chat
-    target = target.replace(/[@]/,'');
+    target = target.replace(/[@]/, '');
 
     // Add the target to the queue
     q.add(target);
-        
+
     // Brand spankin' new queue system to queue up
     // shout outs so they don't happen consecutively.
-    if(!q.isBusy()) {
+    if (!q.isBusy()) {
       q.setBusy(true);
-      while(!q.isEmpty()) {
+      while (!q.isEmpty()) {
         debug(`Iterating Queue.`);
         // Do the shout out
         var streamer = q.first();
@@ -93,50 +94,50 @@ window.addEventListener('onEventReceived', async (obj) => {
   }
 });
 
-async function ShoutOut (username) {
+async function ShoutOut(username) {
   const name = username.toLowerCase();
-  
+
   // Get the user's avatar
   var avatar = GetAvatar(name);
-  
+
   // If an avatar was found...
-  if(avatar) {
+  if (avatar) {
     //Play the video loaded in config
-    if(videoFile !== null)
-    	playVideo(videoFile, videoVolume);
-    
+    if (videoFile !== null)
+      playVideo(videoFile, videoVolume);
+
     //Play the sound loaded in config
-    if(soundFile !== null)
-    	playAudio(soundFile, soundVolume);
-    
+    if (soundFile !== null)
+      playAudio(soundFile, soundVolume);
+
     // Set the user's avatar into the img object
     SetImage(avatar);
-    
-    var TopText = ReplacePseudoVariables('{shoutTopText}', username);
-    var BotText = ReplacePseudoVariables('{shoutBotText}', username);
-    
+
+    var TopText = ReplacePseudoVariables(shoutTopText, username);
+    var BotText = ReplacePseudoVariables(shoutBotText, username);
+
     SetText(TopText, text_main);
     SetText(BotText, text_sub);
-    
+
     // Animate In
     AnimateCSS(avatar_image, "roll-in-blurred-top");
     AnimateCSS(text_main, "tracking-in-contract-bck-top");
     AnimateCSS(text_sub, "tracking-in-contract-bck-bottom");
-    
+
     await sleep(shoutoutDuration);
-    
+
     // Animate Out
     AnimateCSS(avatar_image, "slide-out-blurred-top");
     AnimateCSS(text_main, "text-blur-out");
     AnimateCSS(text_sub, "text-blur-out");
   }
-  
+
   // Wait for the exit animation to complete.
   await sleep(2000);
-  
+
   // Reset the element for the next time the command is used.
   ResetForNextRun();
-  
+
   return Promise.resolve("success");
 }
 
@@ -150,7 +151,7 @@ function ReplacePseudoVariables(text, target = "") {
   debug(`Doing pseudo variable replacement on ${text}.`);
   text = text.replace('[name]', target);
   text = text.replace('[random]', RandomMessage());
-  
+
   return text;
 }
 
@@ -158,7 +159,7 @@ function RandomMessage() {
   return messages[Math.floor(Math.random() * messages.length)];
 }
 
-function GetAvatar (username) {
+function GetAvatar(username) {
   var data = null;
   var xhr = new XMLHttpRequest();
   let result = false;
@@ -173,12 +174,12 @@ function GetAvatar (username) {
   }
   if (xhr.status == 200) {
     let avatarFound = !(xhr.responseText.includes("User not found") || xhr.responseText.includes("No user with the name"));
-    
+
     debug(`Got response from decapi, Avatar Found = ${avatarFound}.`);
 
     if (avatarFound) {
       debug(`Got avatar: ${xhr.responseText}`);
-      
+
       return xhr.responseText;
     } else {
       debug(`Avatar not found. Response from server: ${xhr.responseText}`);
@@ -196,7 +197,7 @@ function SetImage(avatarURL, sizePercentage = 100, posFromTopPercentage = 0) {
 
   // Position the image virtically
   //document.getElementById("main_container").style.top = posFromTopPercentage + "%"
-  
+
   // Set the image size
   avatar_image.style.height = sizePercentage + "%";
 }
@@ -212,23 +213,23 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function ResetForNextRun() {  
+function ResetForNextRun() {
   // Remove the avatar image
   avatar_image.setAttribute("src", '');
-  
+
   // Empty the text strings
   text_main.innerHTML = '';
   text_sub.innerHTML = '';
-  
+
   //Remove the animation classes
   avatar_image.className = '';
   text_main.className = '';
   text_sub.className = '';
 }
 
-function playAudio(sound, volume){
+function playAudio(sound, volume) {
   debug(`Playing sound ${sound} at ${volume} volume.`);
-  let audio = new Audio(sound);	
+  let audio = new Audio(sound);
   audio.volume = volume * .01;
   audio.play();
 }
@@ -239,8 +240,8 @@ function playVideo(video, volume, size = 100) {
   var soundVolume = volume;
 
   document.querySelector("video").setAttribute("src", video);
-  document.querySelector("video").style.height = size+"%";
-  document.querySelector("video").style.width = size+"%";
+  document.querySelector("video").style.height = size + "%";
+  document.querySelector("video").style.width = size + "%";
 
   vid.volume = soundVolume * .01;
 
@@ -248,7 +249,7 @@ function playVideo(video, volume, size = 100) {
 }
 
 function debug(text) {
-  if(debugOutput == true)
+  if (debugOutput == true)
     console.log("DEBUG: " + text);
 }
 
@@ -259,32 +260,32 @@ function Queue() {
   this.busy = false;
 }
 
-Queue.prototype.isBusy = function() {
+Queue.prototype.isBusy = function () {
   return this.busy
 }
 
-Queue.prototype.setBusy = function(status) {
+Queue.prototype.setBusy = function (status) {
   this.busy = status;
 }
 
-Queue.prototype.add = function(record) {
+Queue.prototype.add = function (record) {
   this.data.push(record);
 }
 
-Queue.prototype.remove = function() {
+Queue.prototype.remove = function () {
   this.data.shift();
 }
 
-Queue.prototype.first = function() {
+Queue.prototype.first = function () {
   var first = this.data[0];
   this.remove();
   return first;
 }
 
-Queue.prototype.size = function() {
+Queue.prototype.size = function () {
   return this.data.length;
 }
 
-Queue.prototype.isEmpty = function() {
+Queue.prototype.isEmpty = function () {
   return this.data.length === 0;
 }
